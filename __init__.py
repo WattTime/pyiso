cce39231644fd260b5c190afda846f0c5a4948e4
anna <annarschneider@gmail.com>
2014-02-20 15:32:23 -0800

    more flexible client factory

--- __init__.py
+++ __init__.py
@@ -1,19 +1,28 @@
-from isone import ISONEClient
-from miso import MISOClient
-from spp import SPPClient
-from bpa import BPAClient
-
+import imp
+import os.path
 
 def client_factory(client_name, **kwargs):
     """Return a client for an external data set"""
-    if client_name == 'ISONE':
-        return ISONEClient(**kwargs)
-    if client_name == 'MISO':
-        return MISOClient(**kwargs)
-    if client_name == 'SPP':
-        return SPPClient(**kwargs)
-    if client_name == 'BPA':
-        return BPAClient(**kwargs)
-    else:
+    # set up
+    module_name = client_name.lower()
+    class_name = '%sClient' % client_name.upper()
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    
+    # find module
+    fp, pathname, description = imp.find_module(module_name, [dir_name])
+
+    # load
+    try:
+        mod = imp.load_module(module_name, fp, pathname, description)
+    finally:
+        # Since we may exit via an exception, close fp explicitly.
+        if fp:
+            fp.close()
+            
+    # instantiate class
+    try:
+        client_inst = getattr(mod, class_name)(**kwargs)
+    except AttributeError:
         raise ValueError('No client found for name %s' % client_name)
         
+    return client_inst
