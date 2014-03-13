8ee79af289bf5a1091255ab1fd8b437d62c17bb1
anna <annarschneider@gmail.com>
2014-03-12 17:35:37 -0700

    add warning to logs on no new data

--- tasks.py
+++ tasks.py
@@ -1,10 +1,24 @@
 from __future__ import absolute_import
 from celery import shared_task
 from apps.clients import client_factory
+import logging
+from datetime import datetime
+
+
+# set up logger
+logger = logging.getLogger(__name__)
 
 @shared_task
 def get_generation(ba_name, **kwargs):
     # get data
     c = client_factory(ba_name)
     data = c.get_generation(**kwargs)
+    
+    # log
+    if len(data) == 0:
+        msg = 'No data in %s at %s with args %s' % (ba_name, datetime.utcnow().isoformat(),
+                                                    kwargs)
+        logger.warn(msg)
+    
+    # return
     return data
