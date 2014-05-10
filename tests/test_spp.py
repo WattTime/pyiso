02a71a4d7cd37ad0b08536141d5ffd159103914e
anna <annarschneider@gmail.com>
2014-05-10 08:42:05 -0700

    turn off last SPP tests

--- tests/test_spp.py
+++ tests/test_spp.py
@@ -3,18 +3,18 @@ from unittest import TestCase
 import logging
 
 
-class TestSPP(TestCase):
-    def setUp(self):
-        self.c = client_factory('SPP')
-        handler = logging.StreamHandler()
-        self.c.logger.addHandler(handler)
-        self.c.logger.setLevel(logging.DEBUG)
+# class TestSPP(TestCase):
+#     def setUp(self):
+#         self.c = client_factory('SPP')
+#         handler = logging.StreamHandler()
+#         self.c.logger.addHandler(handler)
+#         self.c.logger.setLevel(logging.DEBUG)
 
-    def test_auth_keys(self):
-        auth, idstr, headers = self.c.auth_keys()
-        self.assertEqual(len(auth), 8)
-        self.assertEqual(len(idstr), 12+50)
-        self.assertIn('set-cookie', headers)
+#     def test_auth_keys(self):
+#         auth, idstr, headers = self.c.auth_keys()
+#         self.assertEqual(len(auth), 8)
+#         self.assertEqual(len(idstr), 12+50)
+#         self.assertIn('set-cookie', headers)
 
     # def test_fetch_csv(self):
     #     self.c.handle_options(data='gen')
