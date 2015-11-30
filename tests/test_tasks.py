c197cedc68013a50903ef35931af03eef1f6f8ea
anna <annarschneider@gmail.com>
2015-11-30 13:00:20 -0800

    fix tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -11,7 +11,7 @@ class TestGenerationTask(TestCase):
         self.forecast_kwargs = {'start_at': now + timedelta(minutes=20),
                                 'end_at': now + timedelta(days=1)}
 
-    def run_test(self, ba, kwargs):
+    def _run_test(self, ba, kwargs):
         expected = client_factory(ba).get_generation(**kwargs)
         received = tasks.get_generation(ba, **kwargs)
         for i in range(len(expected)):
@@ -20,28 +20,28 @@ class TestGenerationTask(TestCase):
                 self.assertEqual(expected[i]['fuel_name'], received[i]['fuel_name'])
 
     def test_bpa_latest(self):
-        self.run_test('BPA', self.latest_kwargs)
+        self._run_test('BPA', self.latest_kwargs)
 
     def test_caiso_latest(self):
-        self.run_test('CAISO', self.latest_kwargs)
+        self._run_test('CAISO', self.latest_kwargs)
 
     def test_caiso_forecast(self):
-        self.run_test('CAISO', self.forecast_kwargs)
+        self._run_test('CAISO', self.forecast_kwargs)
 
     def test_ercot_latest(self):
-        self.run_test('ERCOT', self.latest_kwargs)
+        self._run_test('ERCOT', self.latest_kwargs)
 
     def test_isone_latest(self):
-        self.run_test('ISONE', self.latest_kwargs)
+        self._run_test('ISONE', self.latest_kwargs)
 
     def test_miso_latest(self):
-        self.run_test('MISO', self.latest_kwargs)
+        self._run_test('MISO', self.latest_kwargs)
 
     def test_pjm_latest(self):
-        self.run_test('PJM', self.latest_kwargs)
+        self._run_test('PJM', self.latest_kwargs)
 
     def test_sveri_latest(self):
-        self.run_test('AZPS', self.latest_kwargs)
+        self._run_test('AZPS', self.latest_kwargs)
 
 
 class TestLoadTask(TestCase):
@@ -50,7 +50,7 @@ class TestLoadTask(TestCase):
         now = pytz.utc.localize(datetime.utcnow())
         self.forecast_kwargs = {'start_at': now + timedelta(minutes=20), 'end_at': now + timedelta(days=1)}
 
-    def run_test(self, ba, kwargs):
+    def _run_test(self, ba, kwargs):
         expected = client_factory(ba).get_load(**kwargs)
         received = tasks.get_load(ba, **kwargs)
         for i in range(len(expected)):
@@ -58,40 +58,40 @@ class TestLoadTask(TestCase):
                 self.assertEqual(expected[i]['load_MW'], received[i]['load_MW'])
 
     def test_bpa_latest(self):
-        self.run_test('BPA', self.latest_kwargs)
+        self._run_test('BPA', self.latest_kwargs)
 
     def test_caiso_latest(self):
-        self.run_test('CAISO', self.latest_kwargs)
+        self._run_test('CAISO', self.latest_kwargs)
 
     def test_caiso_forecast(self):
-        self.run_test('CAISO', self.forecast_kwargs)
+        self._run_test('CAISO', self.forecast_kwargs)
 
     def test_isone_latest(self):
-        self.run_test('ISONE', self.latest_kwargs)
+        self._run_test('ISONE', self.latest_kwargs)
 
     def test_isone_forecast(self):
-        self.run_test('ISONE', self.forecast_kwargs)
+        self._run_test('ISONE', self.forecast_kwargs)
 
     def test_pjm_latest(self):
-        self.run_test('PJM', self.latest_kwargs)
+        self._run_test('PJM', self.latest_kwargs)
 
     def test_pjm_forecast(self):
-        self.run_test('PJM', self.forecast_kwargs)
+        self._run_test('PJM', self.forecast_kwargs)
 
     def test_ercot_latest(self):
-        self.run_test('ERCOT', self.latest_kwargs)
+        self._run_test('ERCOT', self.latest_kwargs)
 
     def test_ercot_forecast(self):
-        self.run_test('ERCOT', self.forecast_kwargs)
+        self._run_test('ERCOT', self.forecast_kwargs)
 
     def test_nyiso_latest(self):
-        self.run_test('NYISO', self.latest_kwargs)
+        self._run_test('NYISO', self.latest_kwargs)
 
     def test_nyiso_forecast(self):
-        self.run_test('NYISO', self.forecast_kwargs)
+        self._run_test('NYISO', self.forecast_kwargs)
 
     def test_sveri_latest(self):
-        self.run_test('AZPS', self.latest_kwargs)
+        self._run_test('AZPS', self.latest_kwargs)
 
 
 class TestTradeTask(TestCase):
@@ -100,7 +100,7 @@ class TestTradeTask(TestCase):
         now = pytz.utc.localize(datetime.utcnow())
         self.forecast_kwargs = {'start_at': now + timedelta(minutes=20), 'end_at': now + timedelta(days=1)}
 
-    def run_test(self, ba, kwargs):
+    def _run_test(self, ba, kwargs):
         expected = client_factory(ba).get_trade(**kwargs)
         received = tasks.get_trade(ba, **kwargs)
         for i in range(len(expected)):
@@ -108,13 +108,13 @@ class TestTradeTask(TestCase):
                 self.assertEqual(expected[i]['net_exp_MW'], received[i]['net_exp_MW'])
 
     def test_caiso_latest(self):
-        self.run_test('CAISO', self.latest_kwargs)
+        self._run_test('CAISO', self.latest_kwargs)
 
     def test_caiso_forecast(self):
-        self.run_test('CAISO', self.forecast_kwargs)
+        self._run_test('CAISO', self.forecast_kwargs)
 
     def test_nyiso_latest(self):
-        self.run_test('NYISO', self.latest_kwargs)
+        self._run_test('NYISO', self.latest_kwargs)
 
     def test_miso_forecast(self):
-        self.run_test('MISO', self.forecast_kwargs)
+        self._run_test('MISO', self.forecast_kwargs)
