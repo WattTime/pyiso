c49252233b914ef24723c2d38cb9174b1db23e72
Reid Miller <reid@reidmiller.name>
2016-05-01 23:51:36 -0400

    Initial implementation of a function to parse the Generators Output and Capability report
    IESO's Generators Output and Capability Report is the only report to itemize electricity generation facilities by fuel type and for the current day (Generator Output by Fuel Type Hourly Report only shows past days). The difficulty of this details report is that each facility's output is given. They must be aggregated by fuel type for the purposes of WattTime's API.
    
    I've reduced two copies of the Generators Output and Capability Report (originals are ~2MB each) and stored them as unit test resources. One is a complete day and the second is a partial day. I've done this so that realistic reports are parsed, but the unit tests do not have integration test behaviour (eg. network requests). Existing unit tests have placed report content directly into the python code. Due to the complexity of IESO reports I've simply included them as XML files in a resource directory. (For what it's worth, I feel that existing convention of coding CSV / XLS content into a unit test is a bit goofy.)

--- pyiso/ieso.py
+++ pyiso/ieso.py
@@ -1,3 +1,5 @@
+from lxml import objectify
+
 from pyiso.base import BaseClient
 
 
@@ -7,7 +9,7 @@ class IESOClient(BaseClient):
 
     base_url = 'http://reports.ieso.ca/public/'
 
-    # In Ontario,
+    # In Ontario, references to SOLAR are all solar PV.
     fuels = {
         'NUCLEAR': 'nuclear',
         'GAS': 'natgas',
@@ -20,9 +22,35 @@ class IESOClient(BaseClient):
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
-
     def get_load(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
     def get_trade(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
+
+    def parse_output_capability_report(self, xml_content):
+        """
+        Parse the Generators Output and Capability report, aggregating output hourly by fuel type.
+
+        :param str xml_content: An XML string of the IESO Output Capability report.
+        :return: Dict keyed by fuel and values are generation by hour-of-day
+        :rtype: dict
+        """
+        imo_document = objectify.fromstring(xml_content)
+        imo_doc_body = imo_document.IMODocBody
+        fuels_hourly = {key: list([]) for key in self.fuels.keys()}
+
+        # Iterate over each hourly value for each generator
+        for generator in imo_doc_body.Generators.Generator:
+            fuel_type = generator.FuelType
+            for output in generator.Outputs.Output:
+                hour_of_day = output.Hour - 1 # Hour 1 is output from 00:00:00 to 00:59:59
+                gen_mw = output.EnergyMW
+                fuel_hours = fuels_hourly[fuel_type]
+                try:
+                    fuel_hours[hour_of_day] += gen_mw
+                except IndexError:
+                    # Assumes that hours or processed in order
+                    fuel_hours.append(gen_mw)
+
+        return fuels_hourly
--- tests/test_ieso.py
+++ tests/test_ieso.py
@@ -7,3 +7,34 @@ class TestIESO(TestCase):
     def setUp(self):
         self.c = client_factory('IESO')
 
+    def test_parse_output_capability_report_for_complete_day(self):
+        # Open a simplified, offline copy of complete April 29, 2016 report
+        xml = open('./resources/ieso/reduced_GenOutputCapability_20160429.xml')
+
+        fuels_hourly = self.c.parse_output_capability_report(xml.read())
+
+        self.assertEquals(len(fuels_hourly['NUCLEAR']), 24)
+        self.assertEquals(fuels_hourly['NUCLEAR'][0], 1250)
+        self.assertEquals(fuels_hourly['NUCLEAR'][23], 1555)
+        self.assertEquals(len(fuels_hourly['WIND']), 24)
+        self.assertEquals(fuels_hourly['WIND'][0], 59)
+        self.assertEquals(fuels_hourly['WIND'][23], 26)
+        self.assertEquals(len(fuels_hourly['BIOFUEL']), 24)
+        self.assertEquals(fuels_hourly['BIOFUEL'][0], 0)
+        self.assertEquals(fuels_hourly['BIOFUEL'][23], 0)
+
+    def test_parse_output_capability_report_for_partial_day(self):
+        # Open a simplified, offline copy of partial May 1, 2016 report
+        xml = open('./resources/ieso/reduced_GenOutputCapability_20160501.xml')
+
+        fuels_hourly = self.c.parse_output_capability_report(xml.read())
+
+        self.assertEquals(len(fuels_hourly['NUCLEAR']), 4)
+        self.assertEquals(fuels_hourly['NUCLEAR'][0], 716)
+        self.assertEquals(fuels_hourly['NUCLEAR'][3], 678)
+        self.assertEquals(len(fuels_hourly['GAS']), 4)
+        self.assertEquals(fuels_hourly['GAS'][0], 120)
+        self.assertEquals(fuels_hourly['GAS'][3], 120)
+        self.assertEquals(len(fuels_hourly['HYDRO']), 4)
+        self.assertEquals(fuels_hourly['HYDRO'][0], 165)
+        self.assertEquals(fuels_hourly['HYDRO'][3], 165)
