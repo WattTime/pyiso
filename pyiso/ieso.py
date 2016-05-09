6d9867aeaf600af96ff7276b9618e602f95a00c4
Reid Miller <reid@reidmiller.name>
2016-05-09 01:24:16 -0400

    Changed parse_output_capability_report(...) to return fuel mix format
    The parse_output_capability_report(...) function now returns a list of dictionaries with the fuel mix format matching that of the get_generation(...) function. The idea is that parse_output_capability_report(...) may be called multiple times when stitching multiple days' reports together. I'm not super happy with its structure currently (feels like doing three things in one function). However, I'm putting off refactoring the function until I get a feel for how to encapsulate the functionality given the get_generation(...) function's other options.
    
    I got a start on handling the 'latest' parameter in the get_generation(...) function, but it currently doesn't trim the results. The behaviour of the function does not currently match the description of the base client interface.

--- pyiso/ieso.py
+++ pyiso/ieso.py
@@ -8,6 +8,8 @@ class IESOClient(BaseClient):
     TZ_NAME = 'America/Toronto'
 
     base_url = 'http://reports.ieso.ca/public/'
+    output_capability_url = base_url + 'GenOutputCapability/'
+    output_capability_latest_url = output_capability_url + 'PUB_GenOutputCapability.xml'
 
     # In Ontario, references to SOLAR are all solar PV.
     fuels = {
@@ -20,7 +22,12 @@ class IESOClient(BaseClient):
     }
 
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        if latest:
+            response = self.request(url=self.output_capability_latest_url)
+            fuel_mix = self.parse_output_capability_report(response.content)
+            return fuel_mix
+        else:
+            raise NotImplementedError('Only the latest generation fuel mix data is currently implemented.')
 
     def get_load(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
@@ -30,27 +37,56 @@ class IESOClient(BaseClient):
 
     def parse_output_capability_report(self, xml_content):
         """
-        Parse the Generators Output and Capability report, aggregating output hourly by fuel type.
+        Parse the Generator Output and Capability Report, aggregating output hourly by fuel type.
 
-        :param str xml_content: An XML string of the IESO Output Capability report.
-        :return: Dict keyed by fuel and values are generation by hour-of-day
-        :rtype: dict
+        :param str xml_content: An XML string of the Generator Output and Capability Report.
+        :return: List of dicts, each with keys ``[ba_name, timestamp, freq, market, fuel_name, gen_MW]``.
+           Timestamps are in UTC.
+        :rtype: list
         """
         imo_document = objectify.fromstring(xml_content)
         imo_doc_body = imo_document.IMODocBody
+        report_date = imo_doc_body.Date
         fuels_hourly = {key: list([]) for key in self.fuels.keys()}
 
-        # Iterate over each hourly value for each generator
+        # Iterate over each hourly value for each generator, creating a dictionary keyed by fuel and values are lists
+        # containing generation by hour-of-day.
         for generator in imo_doc_body.Generators.Generator:
             fuel_type = generator.FuelType
             for output in generator.Outputs.Output:
-                hour_of_day = output.Hour - 1 # Hour 1 is output from 00:00:00 to 00:59:59
-                gen_mw = output.EnergyMW
+                hour_of_day = output.Hour - 1  # Hour 1 is output from 00:00:00 to 00:59:59
+                try:
+                    gen_mw = output.EnergyMW
+                except AttributeError:  # Inexplicably, some 'Output' elements are missing 'EnergyMW' child element.
+                    gen_mw = 0
                 fuel_hours = fuels_hourly[fuel_type]
                 try:
                     fuel_hours[hour_of_day] += gen_mw
                 except IndexError:
-                    # Assumes that hours or processed in order
+                    # Assumes that hours are processed in order
                     fuel_hours.append(gen_mw)
 
-        return fuels_hourly
+        # Iterate over aggregated results to create generation fuel mix format
+        fuel_mix = list([])
+        for fuel in fuels_hourly.keys():
+            for idx, fuel_gen_mw in enumerate(fuels_hourly[fuel]):
+                report_ts_local = report_date + ' ' + str(idx).zfill(2) + ':00'
+                report_ts_utc = self.utcify(local_ts_str=report_ts_local, is_dst=False)
+                fuel_mix.append({
+                    'ba_name': self.NAME,
+                    'timestamp': report_ts_utc,
+                    'freq': self.FREQUENCY_CHOICES.hourly,
+                    'market': self.MARKET_CHOICES.hourly,
+                    'fuel_name': self.fuels[fuel],
+                    'gen_MW': fuel_gen_mw
+                })
+
+        return fuel_mix
+
+
+# def main():
+#     client = IESOClient()
+#     client.get_generation(latest=True)
+#
+# if __name__ == '__main__':
+#     main()
--- tests/test_ieso.py
+++ tests/test_ieso.py
@@ -11,30 +11,40 @@ class TestIESO(TestCase):
         # Open a simplified, offline copy of complete April 29, 2016 report
         xml = open('./resources/ieso/reduced_GenOutputCapability_20160429.xml')
 
-        fuels_hourly = self.c.parse_output_capability_report(xml.read())
+        fuel_mix = self.c.parse_output_capability_report(xml.read())
 
-        self.assertEquals(len(fuels_hourly['NUCLEAR']), 24)
-        self.assertEquals(fuels_hourly['NUCLEAR'][0], 1250)
-        self.assertEquals(fuels_hourly['NUCLEAR'][23], 1555)
-        self.assertEquals(len(fuels_hourly['WIND']), 24)
-        self.assertEquals(fuels_hourly['WIND'][0], 59)
-        self.assertEquals(fuels_hourly['WIND'][23], 26)
-        self.assertEquals(len(fuels_hourly['BIOFUEL']), 24)
-        self.assertEquals(fuels_hourly['BIOFUEL'][0], 0)
-        self.assertEquals(fuels_hourly['BIOFUEL'][23], 0)
+        self.assertEquals(len(fuel_mix), 72)  # 24 hours of three fuels
+        for val in fuel_mix:  # Spot check known values
+            if (val['fuel_name'] == 'nuclear') & (val['timestamp'].day == 29) & (val['timestamp'].hour == 4):
+                self.assertEquals(val['gen_MW'], 1250)
+            elif (val['fuel_name'] == 'nuclear') & (val['timestamp'].day == 30) & (val['timestamp'].hour == 3):
+                self.assertEquals(val['gen_MW'], 1555)
+            elif (val['fuel_name'] == 'wind') & (val['timestamp'].day == 29) & (val['timestamp'].hour == 4):
+                self.assertEquals(val['gen_MW'], 59)
+            elif (val['fuel_name'] == 'wind') & (val['timestamp'].day == 30) & (val['timestamp'].hour == 3):
+                self.assertEquals(val['gen_MW'], 26)
+            elif (val['fuel_name'] == 'biomass') & (val['timestamp'].day == 29) & (val['timestamp'].hour == 4):
+                self.assertEquals(val['gen_MW'], 0)
+            elif (val['fuel_name'] == 'biomass') & (val['timestamp'].day == 30) & (val['timestamp'].hour == 3):
+                self.assertEquals(val['gen_MW'], 0)
 
     def test_parse_output_capability_report_for_partial_day(self):
         # Open a simplified, offline copy of partial May 1, 2016 report
         xml = open('./resources/ieso/reduced_GenOutputCapability_20160501.xml')
 
-        fuels_hourly = self.c.parse_output_capability_report(xml.read())
+        fuel_mix = self.c.parse_output_capability_report(xml.read())
 
-        self.assertEquals(len(fuels_hourly['NUCLEAR']), 4)
-        self.assertEquals(fuels_hourly['NUCLEAR'][0], 716)
-        self.assertEquals(fuels_hourly['NUCLEAR'][3], 678)
-        self.assertEquals(len(fuels_hourly['GAS']), 4)
-        self.assertEquals(fuels_hourly['GAS'][0], 120)
-        self.assertEquals(fuels_hourly['GAS'][3], 120)
-        self.assertEquals(len(fuels_hourly['HYDRO']), 4)
-        self.assertEquals(fuels_hourly['HYDRO'][0], 165)
-        self.assertEquals(fuels_hourly['HYDRO'][3], 165)
+        self.assertEquals(len(fuel_mix), 12)  # 4 hours of three fuels
+        for val in fuel_mix:  # Spot check known values
+            if (val['fuel_name'] == 'nuclear') & (val['timestamp'].hour == 4):
+                self.assertEquals(val['gen_MW'], 716)
+            elif (val['fuel_name'] == 'nuclear') & (val['timestamp'].hour == 7):
+                self.assertEquals(val['gen_MW'], 678)
+            elif (val['fuel_name'] == 'natgas') & (val['timestamp'].hour == 4):
+                self.assertEquals(val['gen_MW'], 120)
+            elif (val['fuel_name'] == 'natgas') & (val['timestamp'].hour == 7):
+                self.assertEquals(val['gen_MW'], 120)
+            elif (val['fuel_name'] == 'hydro') & (val['timestamp'].hour == 4):
+                self.assertEquals(val['gen_MW'], 165)
+            elif (val['fuel_name'] == 'hydro') & (val['timestamp'].hour == 7):
+                self.assertEquals(val['gen_MW'], 165)
