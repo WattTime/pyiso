83d6d949b183033ed6d3ac67c843a82df6d52805
Nate <nathan.n.davis@gmail.com>
2016-05-20 18:19:45 -0400

    ERCOT LMP working

--- pyiso/ercot.py
+++ pyiso/ercot.py
@@ -3,6 +3,8 @@ from pyiso.base import BaseClient
 import pandas as pd
 from io import StringIO
 import re
+from datetime import datetime, timedelta
+import pytz
 
 
 class ERCOTClient(BaseClient):
@@ -15,16 +17,24 @@ class ERCOTClient(BaseClient):
         'wind_hrly': '13028',
         'gen_hrly': '12358',
         'load_7day': '12311',
+        'dam_hrly_lmp': '12331',
+        'rt5m_lmp': '12300',
     }
 
     TZ_NAME = 'US/Central'
 
-    def _request_report(self, report_type):
+    def _request_report(self, report_type, date=None):
         # request reports list
         params = {'reportTypeId': self.report_type_ids[report_type]}
         report_list_contents = self.request(self.base_report_url+'/misapp/GetReports.do',
                                             params=params).content
         report_list_soup = BeautifulSoup(report_list_contents)
+
+        # Round minute down to nearest 5 minute period
+        date_format = '%Y%m%d'
+        date = datetime(date.year, date.month, date.day, date.hour,
+                        date.minute - (date.minute % 5), tzinfo=date.tzinfo)
+        date = pytz.timezone(self.TZ_NAME).normalize(date)
 
         # find the endpoint to download
         report_endpoint = None
@@ -32,12 +42,18 @@ class ERCOTClient(BaseClient):
             label = elt.find(class_='labelOptional_ind')
             if label:
                 if 'csv' in label.string:
-                    report_endpoint = self.base_report_url + elt.a.attrs['href']
-                    break
+                    if date and label.string.split('.')[3] == date.strftime(date_format):
+                        if report_type == 'rt5m_lmp' and label.string.split('.')[4].startswith(date.strftime('%H%M')):
+                            report_endpoint = self.base_report_url + elt.a.attrs['href']
+                            break
+                        else:
+                            report_endpoint = self.base_report_url + elt.a.attrs['href']
+                            break
 
         # test endpoint found
         if not report_endpoint:
-            raise ValueError('ERCOT: No report available for %s, soup:\n%s' % (report_type, report_list_soup))
+            raise ValueError(
+                'ERCOT: No report available for %s' % (report_type))
 
         # read report from zip
         r = self.request(report_endpoint)
@@ -174,3 +190,115 @@ class ERCOTClient(BaseClient):
 
         # return
         return data
+
+    def handle_options(self, **kwargs):
+        super(ERCOTClient, self).handle_options(**kwargs)
+
+        # ensure market and freq are set
+        if 'market' not in self.options:
+            if self.options['forecast']:
+                self.options['market'] = self.MARKET_CHOICES.dam
+            else:
+                self.options['market'] = self.MARKET_CHOICES.fivemin
+        if 'freq' not in self.options:
+            if self.options['forecast']:
+                self.options['freq'] = self.FREQUENCY_CHOICES.hourly
+            else:
+                self.options['freq'] = self.FREQUENCY_CHOICES.fivemin
+
+        if 'latest' not in self.options:
+            self.options['latest'] = False
+        if 'forecast' not in self.options:
+            self.options['forecast'] = False
+
+
+    def _parse_dam_times(self, df):
+        # Construct datetime string and convert to naive datetime
+        df['hour'] = (df['HourEnding'].str.replace(':', '.').astype(float) - 1).astype(int)
+        df['datetime_str'] = df['DeliveryDate'] + ' ' + df['hour'].astype(str)
+        date_format = '%m/%d/%Y %H'
+        df.index = pd.to_datetime(df['datetime_str'], format=date_format)
+
+        # convert to local time, ambiguous times fixed by DSTFlag, then to utc
+        df.index = df.index.tz_localize(self.TZ_NAME, ambiguous=df['DSTFlag'] == 'Y')
+        df.drop(['DeliveryDate', 'HourEnding', 'DSTFlag', 'hour', 'datetime_str'], axis=1, inplace=True)
+        df.rename(columns={'SettlementPointPrice': 'lmp', 'SettlementPoint': 'node_id'}, inplace=True)
+        return df
+
+    def _parse_rtm_times(self, df):
+        date_format = '%m/%d/%Y %H:%M:%S'
+        df.index = pd.to_datetime(df['SCEDTimestamp'], format=date_format)
+
+        # When DST ends in the Fall, the repeated hour is NOT in DST
+        df.index = df.index.tz_localize(self.TZ_NAME, ambiguous=df['RepeatedHourFlag'] == 'N')
+        df.drop(['SCEDTimestamp', 'RepeatedHourFlag'], axis=1, inplace=True)
+        df.rename(columns={'LMP': 'lmp', 'SettlementPoint': 'node_id'}, inplace=True)
+        return df
+
+    def format_lmp(self, df):
+
+        if 'SCEDTimestamp' in df.columns:
+            df = self._parse_rtm_times(df)
+        else:
+            df = self._parse_dam_times(df)
+
+        df.index = df.index.tz_convert('utc')
+
+        df['freq'] = self.options['freq']
+        df['ba_name'] = 'ERCOT'
+        df['market'] = self.options['market']
+        df['lmp_type'] = 'prc'
+
+        df.sort_index(inplace=True)
+        df['timestamp'] = df.index
+        return df
+
+    def get_lmp(self, node_id=None, **kwargs):
+        self.handle_options(data='lmp', node_id=node_id, **kwargs)
+
+        if self.options['market'] == self.MARKET_CHOICES.fivemin:
+            report_name = 'rt5m_lmp'
+        elif self.options['market'] == self.MARKET_CHOICES.dam:
+            report_name = 'dam_hrly_lmp'
+        elif self.options['market'] == self.MARKET_CHOICES.hourly:
+            raise NotImplementedError('ERCOT does not produce realtime hourly prices?')
+
+        self.now = datetime.now(pytz.utc)
+
+        if 'start_at' in self.options:
+            # get start and end days in local time
+            tz = pytz.timezone(self.TZ_NAME)
+            start = tz.normalize(self.options['start_at'])
+            start = datetime(start.year, start.month, start.day, tzinfo=start.tzinfo)
+            end = tz.normalize(self.options['end_at'])
+
+            # start one day behind actual start_at, b/c day file named 05/20 contains data for 05/21
+            days_list = [end - timedelta(days=x) for x in range((end-start).days + 2)]
+            pieces = []
+            for day in days_list:
+                try:
+                    pieces.append(self._request_report(report_name, day))
+                except ValueError:
+                    pass
+            report = pd.concat(pieces)
+        else:
+            report = self._request_report(report_name, self.now)
+            if report is None:
+                report = self._request_report(report_name, self.now - timedelta(days=1))
+        df = self.format_lmp(report)
+
+        # strip uneeded times
+        if self.options['latest']:
+            df = df[df.index[-1]:]
+        if self.options['forecast']:
+            df = df[datetime.now(pytz.utc):]
+
+        # strip out unwated nodes
+        if node_id:
+            if not isinstance(node_id, list):
+                node_id = [node_id]
+            reg = re.compile('|'.join(node_id))
+            df = df.ix[df['node_id'].str.contains(reg)]
+
+        return df.to_dict(orient='records')
+
