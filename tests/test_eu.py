d535431be709953e747c3afde4318082a599df65
Marc-Antoine <marc@Marc-Antoines-iMac.local>
2017-05-04 21:02:47 -0700

    Start moving tests that talk to external services to a seperate suite

--- tests/test_eu.py
+++ tests/test_eu.py
@@ -9,9 +9,6 @@ class TestEU(TestCase):
     def setUp(self):
         self.c = client_factory('EU')
 
-    def test_auth(self):
-        self.assertTrue(self.c.auth())
-
     def test_auth_wrongemail(self):
         self.c.session = mock.MagicMock()
         response = mock.MagicMock()
