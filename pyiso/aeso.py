33dd2fdfb3b5ba00ed67770d3c098a4cdc2de492
Reid Miller <reid@reidmiller.name>
2017-07-15 12:40:13 -0400

    #120 Initial implementation for get_generation(latest=True)

--- pyiso/aeso.py
+++ pyiso/aeso.py
@@ -1,10 +1,36 @@
+from StringIO import StringIO
+from datetime import datetime
+
+import pytz
+import mock
+
 from pyiso.base import BaseClient
+from pandas import read_csv
+from pandas import DataFrame
+import warnings
 
 
 class AESOClient(BaseClient):
+    NAME = 'AESO'
+    REPORT_URL = 'http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet?contentType=csv'
+
+    fuels = {
+        'COAL': 'coal',
+        'GAS': 'natgas',
+        'HYDRO': 'hydro',
+        'OTHER': 'other',
+        'WIND': 'wind'
+    }
+
+    TZ_NAME = 'Canada/Mountain'
 
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        if latest:
+            return self._get_latest_report(request_type='generation')
+        else:
+            warnings.warn(message="The AESO client only supports latest=True for retrieving generation fuel mix data.",
+                          category=UserWarning)
+            return None
 
     def get_trade(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
@@ -14,3 +40,49 @@ class AESOClient(BaseClient):
 
     def get_lmp(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
+
+    def _get_latest_report(self, request_type):
+        response = self.request(url=self.REPORT_URL)
+        response_io = StringIO(response.content)
+        response_df = read_csv(response_io, names=['label', 'mc', 'tng', 'dcr'], skiprows=1)
+        if request_type == 'generation':
+            return self._parse_generation(latest_df=response_df)
+        else:
+            raise RuntimeError('Unknown request type: ' + request_type)
+
+    def _parse_generation(self, latest_df):
+        """
+        :param DataFrame latest_df: The latest electricity market report, parsed as a dataframe.
+        :return:
+        """
+        local_dt = self._parse_local_time_from_latest_report(latest_df=latest_df)
+        fuels_df = latest_df.loc[latest_df['label'].isin(self.fuels.keys())]
+
+        generation_df = list([])
+        for idx, row in fuels_df.iterrows():
+            # ``[ba_name, timestamp, freq, market, fuel_name, gen_MW]``
+            generation_df.append({
+                'ba_name': self.NAME,
+                'timestamp': local_dt.astimezone(tz=pytz.UTC),
+                'freq': self.FREQUENCY_CHOICES.fivemin,  # Actually it's every two minutes, but whatever :)
+                'market': self.MARKET_CHOICES.fivemin,
+                'fuel_name': row.label,
+                'gen_MW': float(row.tng)
+            })
+
+        return generation_df
+
+    def _parse_local_time_from_latest_report(self, latest_df):
+        """
+        :param DataFrame latest_df: The latest electricity market report, parsed as a dataframe.
+        :return: The local datetime that the latest electricity market report was published.
+        :rtype datetime
+        """
+        last_update_prefix = "Last Update : "
+        for lbl in latest_df.label:
+            if lbl.startswith(last_update_prefix):
+                local_date_str = lbl.lstrip(last_update_prefix)
+                break
+        local_dt = datetime.strptime(local_date_str, "%b %d, %Y %H:%M")
+        local_dt = local_dt.replace(tzinfo=pytz.timezone(self.TZ_NAME))
+        return local_dt
--- tests/test_aeso.py
+++ tests/test_aeso.py
@@ -1,7 +1,10 @@
+import os
+import requests_mock
 from unittest import TestCase
-
 from pyiso import client_factory
 from pyiso.base import BaseClient
+
+FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 
 
 class TestAESOClient(TestCase):
@@ -10,3 +13,28 @@ class TestAESOClient(TestCase):
 
     def test_aeso_retrievable_from_client_factory(self):
         self.assertIsInstance(self.aeso_client, BaseClient)
+
+    @requests_mock.Mocker()
+    def test_parse_latest_report_for_generation_request(self, req_expectation):
+        csv_content = open(FIXTURES_DIR + '/aeso_latest_electricity_market_report.csv').read().encode('utf8')
+
+        req_expectation.get(self.aeso_client.REPORT_URL, content=csv_content)
+
+        generation_ts = self.aeso_client.get_generation(latest=True)
+
+        self.assertEqual(len(generation_ts), 5)  # Five fuels
+        for row in generation_ts:
+            fuel_name = row.get('fuel_name', None)
+            self.assertNotEqual(fuel_name, None)
+            if fuel_name == 'COAL':
+                self.assertEqual(row.get('gen_MW', None), 4504)
+            elif fuel_name == 'GAS':
+                self.assertEqual(row.get('gen_MW', None), 4321)
+            elif fuel_name == 'HYDRO':
+                self.assertEqual(row.get('gen_MW', None), 426)
+            elif fuel_name == 'OTHER':
+                self.assertTrue(row.get('gen_MW', None), 261)
+            elif fuel_name == 'WIND':
+                self.assertTrue(row.get('gen_MW', None), 542)
+            else:
+                self.fail('Unexpected fuel name found in generation timeseries')
