c1d76518832f022ec41807c871b93b4c8e0f5fbd
Reid Miller <reid@reidmiller.name>
2017-07-16 22:01:39 -0400

    #122 Set TZ_NAME on NBPowerClient.

--- pyiso/nbpower.py
+++ pyiso/nbpower.py
@@ -2,6 +2,8 @@ from pyiso.base import BaseClient
 
 
 class NBPowerClient(BaseClient):
+    TZ_NAME = 'Canada/Atlantic'
+
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
