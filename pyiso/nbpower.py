89d8afb30cc514b9fb938ef0b78aa07a2824505a
Reid Miller <reid@reidmiller.name>
2017-07-17 22:41:48 -0400

    #122 Initial implementations of latest=True for get_load(...) and get_trade(...)
    The parser of the latest HTML report is a delightful soup of tags :)

--- pyiso/nbpower.py
+++ pyiso/nbpower.py
@@ -1,17 +1,110 @@
+import warnings
+
+from bs4 import BeautifulSoup
+from datetime import datetime
 from pyiso.base import BaseClient
 
 
 class NBPowerClient(BaseClient):
+    NAME = 'NBP'
     TZ_NAME = 'Canada/Atlantic'
+    LATEST_REPORT_URL = 'http://tso.nbpower.com/Public/en/SystemInformation_realtime.asp'
 
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
     def get_load(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        if latest:
+            return self._get_latest_report(parser_format=ParserFormat.load)
+        else:
+            warnings.warn(message='The NBPower client only supports latest=True for retrieving net export data.',
+                          category = UserWarning)
+            return None
 
     def get_trade(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        if latest:
+            return self._get_latest_report(parser_format=ParserFormat.trade)
+        else:
+            warnings.warn(message='The NBPower client only supports latest=True for retrieving net export data.',
+                          category = UserWarning)
+            return None
 
     def get_lmp(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
+
+    def _get_latest_report(self, parser_format):
+        response = self.request(url=self.LATEST_REPORT_URL)
+        report_soup = BeautifulSoup(response.content, 'html.parser')
+        report_dt = self._parse_date_from_report(report_soup=report_soup)
+
+        if parser_format == ParserFormat.load:
+            return self._parse_latest_load(report_soup=report_soup, report_dt=report_dt)
+        elif parser_format == ParserFormat.trade:
+            return self._parse_latest_trade(report_soup=report_soup, report_dt=report_dt)
+
+    @staticmethod
+    def _parse_date_from_report(report_soup):
+        """
+        :param BeautifulSoup report_soup:
+        :return: Local datetime parsed from the report.
+        :rtype: datetime
+        """
+        i_element = report_soup.find(name='i')
+        timestamp = i_element.string.split(' Atlantic Time.')[0]
+        local_dt = datetime.strptime(timestamp, '%b %d, %Y %H:%M:%S')
+        return local_dt
+
+    def _parse_latest_load(self, report_soup, report_dt):
+        """
+        :param BeautifulSoup report_soup:
+        :param datetime report_dt:
+        :return: List of dicts, each with keys ``[ba_name, timestamp, freq, market, load_MW]``.
+           Timestamps are in UTC.
+        :rtype: list
+        """
+        # Only td elements aligned center with no bgcolor contain electricity data
+        nbload_td = report_soup.find(name='td', attrs={'align': 'center', 'bgcolor': ''})
+        nbload = float(nbload_td.string)
+        load_ts = [{
+            'ba_name': self.NAME,
+            'timestamp': self.utcify(report_dt),
+            'freq': self.FREQUENCY_CHOICES.fivemin,
+            'market': self.MARKET_CHOICES.fivemin,
+            'load_MW': nbload
+        }]
+        return load_ts
+
+    def _parse_latest_trade(self, report_soup, report_dt):
+        """
+        :param BeautifulSoup report_soup:
+        :param datetime report_dt:
+        :return: List of dicts, each with keys ``[ba_name, timestamp, freq, market, net_exp_MW]``.
+           Timestamps are in UTC.
+        :rtype: list
+        """
+        # Only td elements aligned center with no bgcolor contain electricity data
+        nbtrade_tds = report_soup.find_all(name='td', attrs={'align': 'center', 'bgcolor': ''})
+
+        # Size intertie nodes are elements 2-7
+        nbtrade = 0
+        i = 0
+        for td in nbtrade_tds:
+            if 2 <= i <= 7:
+                nbtrade += float(td.string)
+            i += 1
+
+        load_ts = [{
+            'ba_name': self.NAME,
+            'timestamp': self.utcify(report_dt),
+            'freq': self.FREQUENCY_CHOICES.fivemin,
+            'market': self.MARKET_CHOICES.fivemin,
+            'net_exp_MW': nbtrade
+        }]
+        return load_ts
+
+
+class ParserFormat(object):
+    generation = 'generation'
+    load = 'load'
+    trade = 'trade'
+    lmp = 'lmp'
--- tests/test_nbpower.py
+++ tests/test_nbpower.py
@@ -1,7 +1,12 @@
+import os
 from unittest import TestCase
+
+import requests_mock
 
 from pyiso import client_factory
 from pyiso.base import BaseClient
+
+FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 
 
 class TestNBPowerClient(TestCase):
@@ -10,3 +15,23 @@ class TestNBPowerClient(TestCase):
 
     def test_nbpower_from_client_factory(self):
         self.assertIsInstance(self.nbpower_client, BaseClient)
+
+    @requests_mock.Mocker()
+    def test_get_load_latest(self, req_expectation):
+        html_content = open(FIXTURES_DIR + '/nbpower_SystemInformation_realtime.html').read().encode('utf8')
+        req_expectation.get(self.nbpower_client.LATEST_REPORT_URL, content=html_content)
+
+        load_ts = self.nbpower_client.get_load(latest=True)
+
+        self.assertEqual(len(load_ts), 1)
+        self.assertEqual(load_ts[0].get('load_MW', None), 1150)
+
+    @requests_mock.Mocker()
+    def test_get_trade_latest(self, req_expectation):
+        html_content = open(FIXTURES_DIR + '/nbpower_SystemInformation_realtime.html').read().encode('utf8')
+        req_expectation.get(self.nbpower_client.LATEST_REPORT_URL, content=html_content)
+
+        trade_ts = self.nbpower_client.get_trade(latest=True)
+
+        self.assertEqual(len(trade_ts), 1)
+        self.assertEqual(trade_ts[0].get('net_exp_MW', None), 183)
