09e460c47580d698f4d218c4ea8639da613c7a34
Reid Miller <reid@reidmiller.name>
2017-08-20 11:48:35 -0400

    #130 Removed wind_xls variable from BPA unit test.
    It was not used anywhere in the unit tests and it is not Excel content
    as the file extension suggests.

--- tests/test_bpa.py
+++ tests/test_bpa.py
@@ -1,6 +1,5 @@
 import os
 from datetime import datetime
-from io import StringIO
 from unittest import TestCase
 
 import pandas as pd
@@ -14,45 +13,6 @@ FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 class TestBPABase(TestCase):
     def setUp(self):
         self.wind_tsv = open(FIXTURES_DIR + '/bpa/wind_tsv.csv').read().encode('utf8')
-
-        self.wind_xls = StringIO(u"TOTAL LOAD & WIND GENERATION IN THE BPA CONTROL AREA (Balancing Authority Area)                      \n\
-Beginning 1/1/2014, at 5-min increments, updated daily                      \n\
-This data pertains to the Total Load & Total Wind, Hydro, and Thermal Generation in the BPA Balancing Authority area (control area).                        \n\
-It is based upon 5-minute interpolated snapshots.                       \n\
-                        \n\
-The Balancing Authority load reflects the load in our electrical control area, within which our AGC systems balance generation, interchange, and load.                      \n\
-This is the most common building block unit for load analysis within NWPP, WECC, and NERC.                      \n\
-For wind plants with output split between multiple control areas (e.g., Nine Canyon) only the portion of the output\n\ in the BPA BA is included.                      \n\
-This file includes the Total Wind Generation \"Basepoint\", which is the sum of all operator-supplied wind gen forecasts (schedules).                     \n\
-                        \n\
-Source: 5-min data via SCADA/PI point AGC_ONLN.CAREA..MW.103349 (Total Wind Basepoint)                      \n\
-Source: 5-min data via SCADA/PI point AGCDISTB.AGCRPT..MW.79687 (Total Wind Generation)                     \n\
-Source: 5-min data via SCADA/PI point BPA.CAREA..MW.45583 (Net Load)                        \n\
-Source: 5-min data via SCADA/PI point AGCDISTB.AGCRPT..MW.79682 (Total Hydro Generation)                       \n\
-Source: 5-min data via SCADA/PI point AGCDISTB.AGCRPT..MW.79685 (Total Thermal Generation)                      \n\
-Source: 5-min data via SCADA/PI point AGCDISTB.AGCRPT..MW.45581 (Net Interchange)                       \n\
-BPA Transmission Technical Operations/TOT/21Apr14                       \n\
-                        \n\
-Date/Time   TOTAL WIND GENERATION  BASEPOINT (FORECAST) IN BPA CONTROL AREA (MW; SCADA 103349)\tTOTAL WIND GENERATION  IN BPA CONTROL AREA (MW; SCADA 79687)\tTOTAL BPA CONTROL AREA LOAD (MW; SCADA 45583)\tTOTAL HYDRO GENERATION (MW; SCADA 79682)\tTOTAL THERMAL GENERATION (MW; SCADA 79685)\tNET INTERCHANGE (MW; SCADA 45581)\n\
-01/01/14 00:00\t227\t127\t5965\t5500\t3637\t3299\n\
-01/01/14 00:05\t190\t121\t5977\t5336\t3639\t3119\n\
-01/01/14 00:10\t162\t113\t5936\t5157\t3644\t2977\n\
-01/01/14 00:15\t162\t113\t5907\t5168\t3639\t3011\n\
-01/01/14 00:20\t162\t104\t5908\t5092\t3640\t2929\n\
-01/01/14 00:25\t162\t98\t5918\t5078\t3640\t2898\n\
-01/01/14 00:30\t162\t85\t5879\t5124\t3642\t2973\n\
-01/01/14 00:35\t163\t78\t5858\t5125\t3642\t2988\n\
-01/01/14 00:40\t163\t66\t5900\t5118\t3653\t2937\n\
-01/01/14 00:45\t163\t59\t5886\t5112\t3657\t2943\n\
-01/01/14 00:50\t163\t54\t5878\t5110\t3648\t2935\n\
-01/01/14 00:55\t141\t45\t5880\t5041\t3652\t2811\n\
-01/01/14 01:00\t123\t39\t5865\t5015\t3652\t2745\n\
-01/01/14 01:05\t105\t40\t5870\t5018\t3644\t2685\n\
-01/01/14 01:10\t93\t44\t5868\t5044\t3656\t2678\n\
-01/01/14 01:15\t93\t44\t5853\t5085\t3668\t2745\n\
-01/01/14 01:20\t93\t35\t5854\t5077\t3651\t2710\n\
-01/01/14 01:25\t93\t27\t5852\t5042\t3658\t2676\n\
-")
 
     def test_request_latest(self):
         c = client_factory('BPA')
