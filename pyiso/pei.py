e62239e782aa078e7cbb5f6d25f73dfd7389cd4a
Reid Miller <reid@reidmiller.name>
2017-09-25 06:41:01 -0400

    #129 Implemented get_load(latest=True) for PEI

--- pyiso/pei.py
+++ pyiso/pei.py
@@ -1,18 +1,57 @@
+import json
+import warnings
+import pytz
+from datetime import datetime
 from pyiso.base import BaseClient
 
 
 class PEIClient(BaseClient):
     NAME = 'PEI'
-    TZ_NAME = 'Canada/Atlantic'
+    TZ_NAME = 'Etc/GMT+4'  # Times are always given in Atlantic Standard Time
+
+    def __init__(self):
+        super(PEIClient, self).__init__()
+        self.pei_tz = pytz.timezone(self.TZ_NAME)
+        self.chart_values_url = 'http://www.gov.pe.ca/windenergy/chart-values.php'
 
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
     def get_load(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        self.handle_options(latest=latest, yesterday=yesterday, start_at=start_at, end_at=end_at, **kwargs)
+        if latest:
+            return self.get_latest_load()
+        else:
+            warnings.warn(message='PEIPowerClient only supports the latest=True argument for retrieving load data.',
+                          category=UserWarning)
 
     def get_trade(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
     def get_lmp(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
+
+    def get_latest_load(self):
+        """
+        Requests the JSON backing PEI's public "Wind Energy" page (http://www.gov.pe.ca/windenergy/chart.php)
+        and returns it in pyiso load format.
+        :return: List of dicts, each with keys ``[ba_name, timestamp, freq, market, load_MW]``.
+            Timestamps are in UTC.
+        :rtype: list
+        """
+        loads = []
+        response = self.request(self.chart_values_url)
+        if response:
+            sysload_list = json.loads(response.content.decode('utf-8'))
+            sysload_json = sysload_list[0]
+            seconds_epoch = int(sysload_json.get('updateDate', None))
+            last_updated = self.pei_tz.localize(datetime.fromtimestamp(seconds_epoch))
+            total_on_island_load = float(sysload_json.get('data1', None))
+            loads.append({
+                'ba_name': self.NAME,
+                'timestamp': last_updated.astimezone(pytz.utc),
+                'freq': self.FREQUENCY_CHOICES.tenmin,  # Actually, it's been ~20 minutes pretty consistently.
+                'market': self.MARKET_CHOICES.tenmin,
+                'load_MW': total_on_island_load
+            })
+        return loads
--- tests/unit/test_pei.py
+++ tests/unit/test_pei.py
@@ -1,6 +1,22 @@
+import os
+
+import pytz
+import requests_mock
+from datetime import datetime
 from unittest import TestCase
 from pyiso import client_factory
 from pyiso.base import BaseClient
+
+
+def read_fixture(ba_name, filename):
+    """
+    :param str ba_name: The balancing authority name.
+    :param str filename: The fixture file you wish to load.
+    :return: The file's content.
+    :rtype: str
+    """
+    fixtures_base_path = os.path.join(os.path.dirname(__file__), '../fixtures', ba_name.lower())
+    return open(os.path.join(fixtures_base_path, filename), 'r').read()
 
 
 class TestPEIClient(TestCase):
@@ -9,3 +25,15 @@ class TestPEIClient(TestCase):
 
     def test_pei_retrievable_from_client_factory(self):
         self.assertIsInstance(self.c, BaseClient)
+
+    @requests_mock.Mocker()
+    def test_get_load_parses_json(self, mock_request):
+        expected_response = read_fixture(self.c.NAME, 'chart-values.json').encode('utf8')
+        mock_request.get('http://www.gov.pe.ca/windenergy/chart-values.php', content=expected_response)
+
+        load_ts = self.c.get_load(latest=True)
+
+        self.assertEqual(len(load_ts), 1)
+        self.assertEqual(load_ts[0].get('timestamp', None), datetime(year=2017, month=9, day=25, hour=10, minute=1,
+                                                                     second=1, microsecond=0, tzinfo=pytz.utc))
+        self.assertEqual(load_ts[0].get('load_MW', None), 150.56)
