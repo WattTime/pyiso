f0164b6474252d1aeeaf8da945e48e9fda95c833
Reid Miller <reid@reidmiller.name>
2017-10-11 06:44:34 -0400

    #128 Added tests for YukonEnergyClient date range handling.

--- tests/unit/test_yukon.py
+++ tests/unit/test_yukon.py
@@ -1,11 +1,42 @@
+from datetime import timedelta, datetime
 from unittest import TestCase
+
+import pytz
+from freezegun import freeze_time
+
 from pyiso import client_factory
 from pyiso.base import BaseClient
 
 
+@freeze_time('2017-10-11T10:45:00Z')
 class YukonEnergyClient(TestCase):
     def setUp(self):
+        self.tzaware_utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
         self.c = client_factory('YUKON')
 
     def test_yukon_from_client_factory(self):
         self.assertIsInstance(self.c, BaseClient)
+
+    def test_get_generation_date_range_before_lower_bound_returns_empty(self):
+        start_at = self.tzaware_utcnow - timedelta(hours=26)
+        end_at = self.tzaware_utcnow - timedelta(hours=25)
+        results = self.c.get_generation(start_at=start_at, end_at=end_at)
+        self.assertListEqual(results, [], 'Generation mix results should be empty.')
+
+    def test_get_generation_forecast_returns_empty(self):
+        start_at = self.tzaware_utcnow + timedelta(seconds=1)
+        end_at = self.tzaware_utcnow + timedelta(hours=2)
+        results = self.c.get_generation(start_at=start_at, end_at=end_at)
+        self.assertListEqual(results, [], 'Generation mix forecast results should be empty.')
+
+    def test_get_load_date_range_before_lower_bound_returns_empty(self):
+        start_at = self.tzaware_utcnow - timedelta(hours=26)
+        end_at = self.tzaware_utcnow - timedelta(hours=25)
+        results = self.c.get_load(start_at=start_at, end_at=end_at)
+        self.assertListEqual(results, [], 'Load results should be empty.')
+
+    def test_get_load_forecast_returns_empty(self):
+        start_at = self.tzaware_utcnow + timedelta(seconds=1)
+        end_at = self.tzaware_utcnow + timedelta(hours=2)
+        results = self.c.get_load(start_at=start_at, end_at=end_at)
+        self.assertListEqual(results, [], 'Load forecast results should be empty.')
