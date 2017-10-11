1005478e97cabc1a1ff81c6aa19c4b113265d9c7
Reid Miller <reid@reidmiller.name>
2017-10-11 06:38:38 -0400

    #128 Borrowed private method structure from NSPowerClient.
    It's a little bit copy/paste but the two implementations should start to
    diverge from here.

--- pyiso/yukon.py
+++ pyiso/yukon.py
@@ -1,3 +1,8 @@
+from datetime import timedelta
+
+import pytz
+
+from pyiso import LOGGER
 from pyiso.base import BaseClient
 
 
@@ -5,14 +10,141 @@ class YukonEnergyClient(BaseClient):
     NAME = 'YUKON'
     TZ_NAME = 'Canada/Yukon'
 
+    # Their 'thermal' fuel represents both diesel and natural gas:
+    # http://yukonenergy.ca/energy-in-yukon/electricity-101/quick-facts
+    fuels = {
+        'hydro': 'hydro',
+        'thermal': 'thermal'
+    }
+
+    def __init__(self):
+        super(YukonEnergyClient, self).__init__()
+        self.atlantic_tz = pytz.timezone(self.TZ_NAME)
+        self.base_url = 'http://www.yukonenergy.ca/consumption/'
+        self.current_url = self.base_url + 'chart_current.php?chart=current'
+        self.hourly_url = self.base_url + 'chart.php?chart=hourly'
+        self.yukon_now = self.local_now()
+
+    def handle_options(self, **kwargs):
+        super(YukonEnergyClient, self).handle_options(**kwargs)
+
+        start_of_hour = self.yukon_now.replace(minute=0, second=0, microsecond=0)
+        self.options['earliest_data_at'] = start_of_hour - timedelta(hours=24)
+        self.options['latest_data_at'] = start_of_hour
+
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        self.handle_options(latest=latest, yesterday=yesterday, start_at=start_at, end_at=end_at, data='gen', **kwargs)
+        genmix = []
+        if latest:
+            self._generation_latest(genmix)
+        elif self._is_valid_date_range():
+            self._generation_range(genmix)
+        else:
+            if self.options.get('forecast', False):
+                LOGGER.warn(self.NAME + ': Generation mix forecasts are not supported.')
+            else:
+                msg = '%s: Requested date range %s to %s is outside range of available data from %s to %s.' % \
+                      (self.NAME, self.options.get('start_at', None), self.options.get('end_at', None),
+                       self.options.get('earliest_data_at', None), self.options.get('latest_data_at', None))
+                LOGGER.warn(msg)
+        return genmix
 
     def get_load(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        self.handle_options(latest=latest, yesterday=yesterday, start_at=start_at, end_at=end_at, data='load', **kwargs)
+        loads = []
+        if latest:
+            self._load_latest(loads)
+        elif self._is_valid_date_range():
+            self._load_range(loads)
+        else:
+            if self.options.get('forecast', False):
+                LOGGER.warn(self.NAME + ': Load forecasts are not supported.')
+            else:
+                msg = '%s: Requested date range %s to %s is outside range of available data from %s to %s.' % \
+                      (self.NAME, self.options.get('start_at', None), self.options.get('end_at', None),
+                       self.options.get('earliest_data_at', None), self.options.get('latest_data_at', None))
+                LOGGER.warn(msg)
+        return loads
 
     def get_trade(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
     def get_lmp(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
+        pass
+
+    def _append_generation(self, result_ts, fuel, tz_aware_dt, gen_mw):
+        """
+        Appends a dict to the results list, with the keys [ba_name, timestamp, freq, market, fuel_name, gen_MW].
+        Timestamps are in UTC.
+        :param list result_ts: The timeseries (a list of dicts) which results should be appended to.
+        :param str fuel: WattTime fuel name.
+        :param datetime tz_aware_dt: The datetime of the data being appended (timezone-aware).
+        :param float gen_mw: Electricity generation in megawatts (MW)
+        """
+        result_ts.append({
+            'ba_name': self.NAME,
+            'timestamp': tz_aware_dt.astimezone(pytz.utc),
+            'freq': self.FREQUENCY_CHOICES.hourly,
+            'market': self.MARKET_CHOICES.hourly,
+            'fuel_name': fuel,
+            'gen_MW': gen_mw
+        })
+
+    def _append_load(self, result_ts, tz_aware_dt, load_mw):
+        """
+        Appends a dict to the results list, with the keys [ba_name, timestamp, freq, market, load_MW].
+        Timestamps are in UTC.
+        :param list result_ts: The timeseries (a list of dicts) which results should be appended to.
+        :param datetime tz_aware_dt: The datetime of the data being appended (timezone-aware).
+        :param float load_mw: Electricity load in megawatts (MW)
+        """
+        result_ts.append({
+            'ba_name': self.NAME,
+            'timestamp': tz_aware_dt.astimezone(pytz.utc),
+            'freq': self.FREQUENCY_CHOICES.hourly,
+            'market': self.MARKET_CHOICES.hourly,
+            'load_MW': load_mw
+        })
+
+    def _generation_latest(self, genmix):
+        """
+        Requests the latest electricity generation mix data and appends results in pyiso format to the provided list.
+        :param list genmix: The pyiso results list to append results to.
+        """
+        pass
+
+    def _generation_range(self, genmix):
+        """
+        Requests historical generation mix data and appends results in pyiso format to the provided list for those
+        results which fall between start_at and end_at time range.
+        :param list genmix: The pyiso results list to append results to.
+        """
+        pass
+
+    def _is_valid_date_range(self):
+        """
+        Checks whether the start_at and end_at options provided lie within the supported date range. Assumes that
+        self.handle_options(...) has already been called.
+        :return: True/False indicating whether to requested date range is valid.
+        :rtype: bool
+        """
+        if self.options['start_at'] > self.options['latest_data_at'] or \
+           self.options['end_at'] < self.options['earliest_data_at']:
+            return False
+        else:
+            return True
+
+    def _load_range(self, loads):
+        """
+        Requests historical electricity loads and appends results in pyiso format to the provided list for those
+        results which fall between start_at and end_at time range.
+        :param list loads: The pyiso results list to append results to.
+        """
+        pass
+
+    def _load_latest(self, loads):
+        """
+        Requests the latest electricity loads and appends results in pyiso format to the provided list.
+        :param list loads: The pyiso results list to append results to.
+        """
         pass
