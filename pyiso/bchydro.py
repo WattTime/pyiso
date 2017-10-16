3217cd43594af9855c93a909bc6d4ce20f1fc8d6
Reid Miller <reid@reidmiller.name>
2017-10-16 07:07:33 -0400

    #147 Added earliest/latest date range handling.

--- pyiso/bchydro.py
+++ pyiso/bchydro.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 import pytz
 
+from pyiso import LOGGER
 from pyiso.base import BaseClient
 
 
@@ -12,6 +15,11 @@ class BCHydroClient(BaseClient):
         self.bc_tz = pytz.timezone(self.TZ_NAME)
         self.bc_now = self.local_now()
 
+    def handle_options(self, **kwargs):
+        super(BCHydroClient, self).handle_options(**kwargs)
+        self.options['earliest_data_at'] = self.bc_now - timedelta(days=9)
+        self.options['latest_data_at'] = self.bc_now
+
     def get_generation(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
         pass
 
@@ -19,7 +27,40 @@ class BCHydroClient(BaseClient):
         pass
 
     def get_trade(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
-        pass
+        self.handle_options(latest=latest, yesterday=yesterday, start_at=start_at, end_at=end_at, data='trade')
+        trades = []
+        if latest:
+            self._trade_latest()
+        elif self._is_valid_date_range():
+            self._trade_historical()
+        else:
+            if self.options.get('forecast', False):
+                LOGGER.warn(self.NAME + ': Trade forecasts are not supported.')
+            else:
+                msg = '%s: Requested date range %s to %s is outside range of available data from %s to %s.' % \
+                      (self.NAME, self.options.get('start_at', None), self.options.get('end_at', None),
+                       self.options.get('earliest_data_at', None), self.options.get('latest_data_at', None))
+                LOGGER.warn(msg)
+        return trades
 
     def get_lmp(self, latest=False, yesterday=False, start_at=False, end_at=False, **kwargs):
+        pass
+
+    def _is_valid_date_range(self):
+        """
+        Checks whether the start_at and end_at options provided lie within the supported date range. Assumes that
+        self.handle_options(...) has already been called.
+        :return: True/False indicating whether to requested date range is valid.
+        :rtype: bool
+        """
+        if self.options['start_at'] > self.options['latest_data_at'] or \
+           self.options['end_at'] < self.options['earliest_data_at']:
+            return False
+        else:
+            return True
+
+    def _trade_historical(self):
+        pass
+
+    def _trade_latest(self):
         pass
--- tests/unit/test_bchydro.py
+++ tests/unit/test_bchydro.py
@@ -1,12 +1,26 @@
+import pytz
 from unittest import TestCase
-
+from datetime import datetime, timedelta
 from pyiso import client_factory
 from pyiso.base import BaseClient
 
 
 class TestBCHydroClient(TestCase):
     def setUp(self):
+        self.tzaware_utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
         self.c = client_factory('BCH')
 
-    def test_yukon_from_client_factory(self):
+    def test_bchydro_from_client_factory(self):
         self.assertIsInstance(self.c, BaseClient)
+
+    def test_get_trade_date_range_before_lower_bound_returns_empty(self):
+        start_at = self.tzaware_utcnow - timedelta(days=11)
+        end_at = self.tzaware_utcnow - timedelta(days=10)
+        results = self.c.get_trade(start_at=start_at, end_at=end_at)
+        self.assertListEqual(results, [], 'Trade results should be empty.')
+
+    def test_get_trade_forecast_returns_empty(self):
+        start_at = self.tzaware_utcnow + timedelta(seconds=1)
+        end_at = self.tzaware_utcnow + timedelta(hours=2)
+        results = self.c.get_trade(start_at=start_at, end_at=end_at)
+        self.assertListEqual(results, [], 'Trade forecast results should be empty.')
