5d8c560091634bf59a1a30d25ed31bed0f4cde2e
Reid Miller <reid@reidmiller.name>
2017-10-19 22:49:17 -0400

    #165 Implemented get_load(latest=True) for NLHydroClient.

--- tests/unit/test_nlhydro.py
+++ tests/unit/test_nlhydro.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 
+import requests_mock
+from dateutil.parser import parse
+
 from pyiso import client_factory
 from pyiso.base import BaseClient
+from tests import read_fixture
 
 
 class TestNLHydroClient(TestCase):
@@ -10,3 +14,13 @@ class TestNLHydroClient(TestCase):
 
     def test_nlhydro_from_client_factory(self):
         self.assertIsInstance(self.c, BaseClient)
+
+    @requests_mock.Mocker()
+    def test_get_load_latest(self, mocked_request):
+        expected_response = read_fixture(self.c.__module__, 'system-information-center.html').encode('utf8')
+        mocked_request.get(self.c.SYSTEM_INFO_URL, content=expected_response)
+
+        load_ts = self.c.get_load(latest=True)
+        self.assertEqual(len(load_ts), 1)
+        self.assertEqual(load_ts[0].get('timestamp', None), parse('2017-10-20T01:45:00Z'))
+        self.assertEqual(load_ts[0].get('load_MW', None), 773)
