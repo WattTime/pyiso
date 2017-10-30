179a23a65d28d26ccd3f5d8bf6d12be265449362
Adam Donnison <ajdonnison@gmail.com>
2017-10-30 19:57:19 +1100

    Handle lxml issues with unicode input

--- tests/unit/test_eu.py
+++ tests/unit/test_eu.py
@@ -22,13 +22,13 @@ class TestEU(TestCase):
     def test_parse_load(self):
         self.c.handle_options(latest=True, control_area='DE(TenneT GER)', data='load')
         with open(os.path.join(fixtures_base_path, 'de_load.xml'), 'r') as report:
-          parsed = self.c.parse_response(report.read())
+          parsed = self.c.parse_response(report.read().decode('utf-8').encode('ascii'))
           self.assertEqual(parsed[-1]['load_MW'], 13926)
 
     def test_parse_gen(self):
         self.c.handle_options(latest=True, control_area='DE(TenneT GER)', data='gen')
         with open(os.path.join(fixtures_base_path, 'de_gen.xml'), 'r') as report:
-          parsed = self.c.parse_response(report.read())
+          parsed = self.c.parse_response(report.read().decode('utf-8').encode('ascii'))
           self.assertEqual(parsed[-1]['gen_MW'], 3816)
           self.assertEqual(parsed[-1]['fuel_name'], 'nuclear')
 
