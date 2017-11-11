ac99447489e33022231742019cf9af1049bb62e9
Reid Miller <reid@reidmiller.name>
2017-11-11 07:34:12 -0500

    #177 YukonClient unit tests now only freeze time when necessary.
    This should allow unit tests for different frozen times to be added more easily.

--- tests/unit/test_yukon.py
+++ tests/unit/test_yukon.py
@@ -11,7 +11,6 @@ from pyiso.base import BaseClient
 from tests import read_fixture
 
 
-@freeze_time('2017-10-11T10:45:00Z')
 class YukonEnergyClient(TestCase):
     def setUp(self):
         self.tzaware_utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
@@ -44,25 +43,29 @@ class YukonEnergyClient(TestCase):
         results = self.c.get_load(start_at=start_at, end_at=end_at)
         self.assertListEqual(results, [], 'Load forecast results should be empty.')
 
+    @freeze_time('2017-10-11T10:45:00Z')
     @requests_mock.Mocker()
     def test_get_load_latest_returns_expected(self, mocked_request):
+        frozen_client = client_factory('YUKON')
         expected_url = 'http://www.yukonenergy.ca/consumption/chart_current.php?chart=current'
-        expected_response = read_fixture(self.c.__module__, 'current.html')
+        expected_response = read_fixture(frozen_client.__module__, 'current.html')
         mocked_request.get(expected_url, content=expected_response.encode('utf-8'))
 
-        results = self.c.get_load(latest=True)
+        results = frozen_client.get_load(latest=True)
 
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0]['timestamp'], parse('2017-10-11T10:40:00.000Z'))
         self.assertAlmostEqual(results[0]['load_MW'], 38.74)
 
+    @freeze_time('2017-10-11T10:45:00Z')
     @requests_mock.Mocker()
     def test_get_generation_latest_returns_expected(self, mocked_request):
+        frozen_client = client_factory('YUKON')
         expected_url = 'http://www.yukonenergy.ca/consumption/chart_current.php?chart=current'
-        expected_response = read_fixture(self.c.__module__, 'current.html')
+        expected_response = read_fixture(frozen_client.__module__, 'current.html')
         mocked_request.get(expected_url, content=expected_response.encode('utf-8'))
 
-        results = self.c.get_generation(latest=True)
+        results = frozen_client.get_generation(latest=True)
 
         self.assertEqual(len(results), 2)
         self.assertEqual(results[0]['timestamp'], parse('2017-10-11T10:40:00.000Z'))
@@ -72,15 +75,18 @@ class YukonEnergyClient(TestCase):
         self.assertEqual(results[1]['fuel_name'], 'thermal')
         self.assertAlmostEqual(results[1]['gen_MW'], 0)
 
+    @freeze_time('2017-10-11T10:45:00Z')
     @requests_mock.Mocker()
     def test_get_generation_valid_date_range_during_dst_returns_expected(self, mocked_request):
-        start_at = self.tzaware_utcnow - timedelta(hours=12)
-        end_at = self.tzaware_utcnow
+        frozen_client = client_factory('YUKON')
+        frozen_utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
+        start_at = frozen_utcnow - timedelta(hours=12)
+        end_at = frozen_utcnow
         expected_url = 'http://www.yukonenergy.ca/consumption/chart.php?chart=hourly'
-        expected_response = read_fixture(self.c.__module__, 'hourly_2017-10-11.html')
+        expected_response = read_fixture(frozen_client.__module__, 'hourly_2017-10-11.html')
         mocked_request.get(expected_url, content=expected_response.encode('utf-8'))
 
-        results = self.c.get_generation(start_at=start_at, end_at=end_at)
+        results = frozen_client.get_generation(start_at=start_at, end_at=end_at)
 
         expected_length = 22  # 2 fuels * 11 hours (1 hour is missing in this particular response)
         self.assertEqual(len(results), expected_length)
@@ -93,15 +99,18 @@ class YukonEnergyClient(TestCase):
         self.assertEqual(results[21]['fuel_name'], 'thermal')
         self.assertAlmostEqual(results[21]['gen_MW'], 0)
 
+    @freeze_time('2017-10-11T10:45:00Z')
     @requests_mock.Mocker()
     def test_get_load_valid_date_range_during_dst_returns_expected(self, mocked_request):
-        start_at = self.tzaware_utcnow - timedelta(hours=12)
-        end_at = self.tzaware_utcnow
+        frozen_client = client_factory('YUKON')
+        frozen_utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
+        start_at = frozen_utcnow - timedelta(hours=12)
+        end_at = frozen_utcnow
         expected_url = 'http://www.yukonenergy.ca/consumption/chart.php?chart=hourly'
-        expected_response = read_fixture(self.c.__module__, 'hourly_2017-10-11.html')
+        expected_response = read_fixture(frozen_client.__module__, 'hourly_2017-10-11.html')
         mocked_request.get(expected_url, content=expected_response.encode('utf-8'))
 
-        results = self.c.get_load(start_at=start_at, end_at=end_at)
+        results = frozen_client.get_load(start_at=start_at, end_at=end_at)
 
         expected_length = 11  # 11 hours (1 hour is missing in this particular response)
         self.assertEqual(len(results), expected_length)
@@ -115,7 +124,6 @@ class YukonEnergyClient(TestCase):
     def test_get_trade_latest_returns_zero(self):
         results = self.c.get_trade(latest=True)
         self.assertEqual(len(results), 1)
-        self.assertTrue(results[0]['timestamp'], parse('2017-10-11T10:00:00Z'))
         self.assertEqual(results[0]['net_exp_MW'], 0)
 
     def test_get_trade_date_range_retuns_zeros(self):
@@ -125,7 +133,5 @@ class YukonEnergyClient(TestCase):
         results = self.c.get_trade(start_at=start_at, end_at=end_at)
 
         self.assertEqual(len(results), 12)
-        self.assertTrue(results[0]['timestamp'], parse('2017-10-11T10:00:00Z'))
-        self.assertTrue(results[11]['timestamp'], parse('2017-10-10T23:00:00Z'))
         for result in results:
             self.assertEqual(result['net_exp_MW'], 0)
