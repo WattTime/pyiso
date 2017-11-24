a6ecb72236781c37f03b0fcb190ef57b72e1ddaa
Reid Miller <reid@reidmiller.name>
2017-11-24 16:02:59 -0500

    #181 Converted NS Power 'timestamp' values returned to pandas.Timestamp

--- tests/unit/test_nspower.py
+++ tests/unit/test_nspower.py
@@ -1,11 +1,10 @@
-from datetime import datetime, timedelta
-from unittest import TestCase
-
 import pytz
 import requests_mock
-from dateutil.parser import parse
+from datetime import datetime
+from datetime import timedelta
+from unittest import TestCase
 from freezegun import freeze_time
-
+from pandas import Timestamp
 from pyiso import client_factory
 from pyiso.base import BaseClient
 from tests import read_fixture
@@ -58,10 +57,10 @@ class TestNSPower(TestCase):
         self.assertEqual(len(results), expected_length)
 
         # Spot check values at the start and end of the results
-        self.assertEqual(results[0]['timestamp'], parse('2017-10-05T00:00:00.000Z'))
+        self.assertEqual(results[0]['timestamp'], Timestamp('2017-10-05T00:00:00.000Z'))
         self.assertEqual(results[0]['fuel_name'], 'biomass')
         self.assertAlmostEqual(results[0]['gen_MW'], 1.95)
-        self.assertEqual(results[95]['timestamp'], parse('2017-10-05T11:00:00.000Z'))
+        self.assertEqual(results[95]['timestamp'], Timestamp('2017-10-05T11:00:00.000Z'))
         self.assertEqual(results[95]['fuel_name'], 'wind')
         self.assertAlmostEqual(results[95]['gen_MW'], 35.52)
 
@@ -77,7 +76,7 @@ class TestNSPower(TestCase):
         self.assertEqual(len(results), expected_length)
 
         # Check that all datetime values are equal and known fuel values
-        expected_datetime = parse('2017-10-05T11:00:00.000Z')
+        expected_timestamp = Timestamp('2017-10-05T11:00:00.000Z')
         expected_mw_by_fuel = {
             'coal': 44.91,
             'dual': 14.13,
@@ -89,7 +88,7 @@ class TestNSPower(TestCase):
             'other': 0.25
         }
         for result in results:
-            self.assertEqual(result['timestamp'], expected_datetime)
+            self.assertEqual(result['timestamp'], expected_timestamp)
             expected_gen_mw = expected_mw_by_fuel.get(result['fuel_name'], -1)
             self.assertAlmostEqual(result['gen_MW'], expected_gen_mw)
 
@@ -107,9 +106,9 @@ class TestNSPower(TestCase):
         self.assertEqual(len(results), hours)
 
         # Spot check values at the start and end of the results
-        self.assertEqual(results[0]['timestamp'], parse('2017-10-05T00:00:00.000Z'))
+        self.assertEqual(results[0]['timestamp'], Timestamp('2017-10-05T00:00:00.000Z'))
         self.assertAlmostEqual(results[0]['load_MW'], 877.25)
-        self.assertEqual(results[11]['timestamp'], parse('2017-10-05T11:00:00.000Z'))
+        self.assertEqual(results[11]['timestamp'], Timestamp('2017-10-05T11:00:00.000Z'))
         self.assertAlmostEqual(results[11]['load_MW'], 892.64)
 
     @requests_mock.Mocker()
@@ -121,7 +120,7 @@ class TestNSPower(TestCase):
         results = self.c.get_load(latest=True)
 
         self.assertEqual(len(results), 1)
-        self.assertEqual(results[0]['timestamp'], parse('2017-10-05T11:00:00.000Z'))
+        self.assertEqual(results[0]['timestamp'], Timestamp('2017-10-05T11:00:00.000Z'))
         self.assertAlmostEqual(results[0]['load_MW'], 892.64)
 
     @requests_mock.Mocker()
@@ -138,9 +137,9 @@ class TestNSPower(TestCase):
         self.assertEqual(len(results), hours)
 
         # Spot check values at the start and end of the results
-        self.assertEqual(results[0]['timestamp'], parse('2017-10-05T12:00:00.000Z'))
+        self.assertEqual(results[0]['timestamp'], Timestamp('2017-10-05T12:00:00.000Z'))
         self.assertAlmostEqual(results[0]['load_MW'], 1016)
-        self.assertEqual(results[11]['timestamp'], parse('2017-10-05T23:00:00.000Z'))
+        self.assertEqual(results[11]['timestamp'], Timestamp('2017-10-05T23:00:00.000Z'))
         self.assertAlmostEqual(results[11]['load_MW'], 1020)
 
     @requests_mock.Mocker()
@@ -160,7 +159,7 @@ class TestNSPower(TestCase):
         self.assertEqual(len(results), hours * 2)
 
         # Spot check values at the start and end of the results
-        self.assertEqual(results[0]['timestamp'], parse('2017-10-05T00:00:00.000Z'))
+        self.assertEqual(results[0]['timestamp'], Timestamp('2017-10-05T00:00:00.000Z'))
         self.assertAlmostEqual(results[0]['load_MW'], 877.25)
-        self.assertEqual(results[23]['timestamp'], parse('2017-10-05T23:00:00.000Z'))
+        self.assertEqual(results[23]['timestamp'], Timestamp('2017-10-05T23:00:00.000Z'))
         self.assertAlmostEqual(results[23]['load_MW'], 1020)
