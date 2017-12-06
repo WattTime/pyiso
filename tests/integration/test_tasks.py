f829e94c66b974b5db47dfc5744615e5b4d8612b
Reid Miller <reid@reidmiller.name>
2017-12-05 22:58:24 -0500

    #156 Replace commented references to static responses in TestGenerationTask
    The TestGenerationTask class in test_tasks.py had commented-out references to static responses. The tests are now now dynamic (furthered by #130) so the old comments are no longer relevant.
    
    This commit removes the comments and deletes the now  unused file the comments were about.

--- tests/integration/test_tasks.py
+++ tests/integration/test_tasks.py
@@ -2,9 +2,6 @@ from pyiso import client_factory, tasks
 from unittest import TestCase
 from datetime import datetime, timedelta
 import pytz
-import pandas  # pandas must be imported before freezegun # noqa
-# import requests_mock
-# import freezegun
 
 
 class TestGenerationTask(TestCase):
@@ -46,20 +43,7 @@ class TestGenerationTask(TestCase):
     def test_caiso_past_str(self):
         self._run_test('CAISO', self.past_kwargs_str)
 
-    # @freezegun.freeze_time('2016-05-20 12:45', tz_offset=0, tick=True)
-    # @requests_mock.mock()
     def test_caiso_forecast(self):  # , mocker):
-        # Set up mocking
-        # url = 'http://oasis.caiso.com/oasisapi/SingleZip'
-        # f = open('responses/ENE_SLRS.zip', 'rb')
-        # first_resp = f.read()
-        # f.close()
-        # f = open('responses/SLD_REN_FCST.zip', 'rb')
-        # second_resp = f.read()
-        # f.close()
-        # # get_generation first calls ENE_SLRS, then SLD_REN_FCST; gen_generation() called twice
-        # mocker.get(url, [{'content': first_resp}, {'content': second_resp},
-        #                  {'content': first_resp}, {'content': second_resp}])
         self._run_test('CAISO', self.forecast_kwargs)
 
     def test_ercot_latest(self):
@@ -215,4 +199,3 @@ class TestTradeTask(TestCase):
 
     def test_miso_forecast(self):
         self._run_test('MISO', self.forecast_kwargs)
-
